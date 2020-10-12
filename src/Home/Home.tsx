24eff7184586a591833ec9c43de16e66c7e55caf
Gopal Kataria <gopalkataria@outlook.com>
2020-10-12 16:40:41 +0530

    fixed critical bugs in edit mode, added a bug in meet url

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,22 +1,22 @@
 import React, { useState } from "react";
 import {
-	Grid,
-	Box,
-	Typography,
-	ThemeProvider,
-	createMuiTheme,
-	Card,
-	Dialog,
-	DialogContent,
-	DialogTitle,
-	CardContent,
-	Link,
-	Fab,
-	TextField,
-	DialogActions,
-	Button,
-	CardHeader,
-	IconButton,
+  Grid,
+  Box,
+  Typography,
+  ThemeProvider,
+  createMuiTheme,
+  Card,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  CardContent,
+  Link,
+  Fab,
+  TextField,
+  DialogActions,
+  Button,
+  CardHeader,
+  IconButton,
 } from "@material-ui/core";
 import { red, blue } from "@material-ui/core/colors";
 import { getData, Meet, saveData } from "../scripts/data-storage";
@@ -27,190 +27,234 @@ import { AddSharp, EditSharp, DeleteSharp } from "@material-ui/icons";
 //eslint-disable-next-line
 const URL_REGEX = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+(:[0-9]+)?|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/;
 
-function NewMeetingDialogContents(props: any) {
-	return (
-		<div>
-			<DialogContent>
-				<TextField
-					onBlur={(evt) => {
-						let newMeetCopy = Object.assign({}, props.newMeet);
+function NewMeetingDialogContents(props: {
+  handleDialogClose: { (): void };
+  handleDialogSaveAndClose: { (newMeet: Meet): void };
+  newMeet: Meet;
+  setNewMeet: { (meet: Meet): void };
+  editMode: boolean;
+}) {
 
-						newMeetCopy.name = evt.target.value;
-						props.setnewMeet(newMeetCopy);
-					}}
-					autoFocus
-					label='Meeting Name'
-					fullWidth
-					type='text'
-					inputProps={{ defaultValue: props.newMeet.name }}
-				/>
-				<TextField
-					onBlur={(evt) => {
-						let newMeetCopy = Object.assign({}, props.newMeet);
 
-						newMeetCopy.description = evt.target.value;
-						props.setnewMeet(newMeetCopy);
-					}}
-					multiline
-					label='Meeting Description (optional)'
-					fullWidth
-					type='text'
-					inputProps={{ defaultValue: props.newMeet.description }}
-				/>
+	const [newMeet, setNewMeet] = useState<Meet>(
+    Object.assign({}, props.newMeet)
+  );
+  let _newMeet = Object.assign({}, props.newMeet);
 
-				<TextField
-					onChange={(evt) => {
-						let newMeetCopy = Object.assign({}, props.newMeet);
+  return (
+    <div>
+      <DialogContent>
+        <TextField
+          onBlur={(evt) => {
+					  _newMeet.name = evt.target.value;
+					  setNewMeet(_newMeet);
+          }}
+          autoFocus
+          label="Meeting Name"
+          fullWidth
+          type="text"
+          inputProps={{ defaultValue: props.newMeet.name }}
+        />
+        <TextField
+          onBlur={(evt) => {
+            _newMeet.description = evt.target.value;
+            setNewMeet(_newMeet);
+          }}
+          multiline
+          label="Meeting Description (optional)"
+          fullWidth
+          type="text"
+          inputProps={{ defaultValue: props.newMeet.description }}
+        />
 
-						newMeetCopy.url = evt.target.value;
-						props.setnewMeet(newMeetCopy);
-					}}
-					error={!URL_REGEX.test(props.newMeet.url) && props.newMeet.url !== ""}
-					label='Meeting URL'
-					fullWidth
-					type='url'
-					inputProps={{ defaultValue: props.newMeet.url }}
-				/>
-			</DialogContent>
-			<DialogActions>
-				<Button onClick={props.handleDialogClose}>Cancel</Button>
-				<Button
-					disabled={props.newMeet.url === ""}
-					variant='contained'
-					color='secondary'
-					onClick={() => {
-						props.handleDialogSaveAndClose(props.newMeet);
-					}}>
-					Save
-				</Button>
-			</DialogActions>
-		</div>
-	);
+        <TextField
+          onChange={(evt) => {
+            _newMeet.url = evt.target.value;
+            setNewMeet(_newMeet);
+          }}
+          error={!URL_REGEX.test(props.newMeet.url) && props.newMeet.url !== ""}
+          label="Meeting URL"
+          fullWidth
+          type="url"
+          inputProps={{ defaultValue: props.newMeet.url }}
+        />
+      </DialogContent>
+
+      <DialogActions>
+        <Button
+          onClick={
+            props.editMode
+              ? () => {
+                  props.handleDialogSaveAndClose(props.newMeet);
+                }
+              : props.handleDialogClose
+          }
+        >
+          Cancel
+        </Button>
+        <Button
+          disabled={ newMeet.url === ""}
+          variant="contained"
+          color="secondary"
+          onClick={() => {
+            props.setNewMeet(_newMeet);
+            props.handleDialogSaveAndClose(newMeet);
+          }}
+        >
+          Save
+        </Button>
+      </DialogActions>
+    </div>
+  );
 }
 
 export default function Home() {
-	const theme = createMuiTheme({
-		// carefully curated set of theme colors and fonts, don't change anything here
-		palette: {
-			// type : "dark" , // uncomment this line to switch to dark mode
+  const theme = createMuiTheme({
+    // carefully curated set of theme colors and fonts, don't change anything here
+    palette: {
+      // type : "dark" , // uncomment this line to switch to dark mode
 
-			primary: {
-				main: "#673AB7",
-				contrastText: "#053342",
-			},
-			secondary: {
-				main: "#053342",
-			},
-		},
-	});
+      primary: {
+        main: "#673AB7",
+        contrastText: "#053342",
+      },
+      secondary: {
+        main: "#053342",
+      },
+    },
+  });
 
-	const [dialogOpen, setDialogOpen] = useState(false);
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [editMode, setEditMode] = useState(false);
+  const [newMeet, setNewMeet] = useState<Meet>({
+    name: "",
+    url: "",
+    description: "",
+    uid: 0,
+  });
 
-	const [newMeet, setNewMeet] = useState<Meet>({ name: "", url: "", description: "", uid: 0 });
+  const handleDialogClose = () => {
+    setDialogOpen(false);
+    setEditMode(false);
+  };
+  const handleDialogSaveAndClose = (newMeet: Meet) => {
+    let time = new Date();
+    let newMeetings = [
+      ...meetings,
+      {
+        name: newMeet.name,
+        url: newMeet.url,
+        description: newMeet.description,
+        uid: time.getTime(),
+      },
+    ];
+    setMeetings(newMeetings);
 
-	const handleDialogClose = () => {
-		setDialogOpen(false);
-	};
-	const handleDialogSaveAndClose = (newMeet: Meet) => {
-		let time = new Date();
-		let newMeetings = [
-			...meetings,
-			{
-				name: newMeet.name,
-				url: newMeet.url,
-				description: newMeet.description,
-				uid: time.getTime(),
-			},
-		];
-		setMeetings(newMeetings);
+    saveData(newMeetings);
+    handleDialogClose();
+  };
 
-		saveData(newMeetings);
-		handleDialogClose();
-	};
+  const deleteMeet = (uid: number, Meetings: Meet[]) => {
+    let newMeetings = Meetings.filter((meet) => meet.uid !== uid);
+    setMeetings(newMeetings);
+    saveData(newMeetings);
+  };
+  const editMeet = (uid: number, Meetings: Meet[]) => {
+    setNewMeet(Meetings.filter((meet) => meet.uid === uid)[0]);
+    setDialogOpen(true);
+    setEditMode(true);
+    let newMeetings = Meetings.filter((meet) => meet.uid !== uid);
+    setMeetings(newMeetings);
+    saveData(newMeetings);
+  };
 
-	const deleteMeet = (uid: number, Meetings: Meet[]) => {
-		let newMeetings = Meetings.filter((meet) => meet.uid !== uid);
-		setMeetings(newMeetings);
-		saveData(newMeetings);
-	};
-	const editMeet = (uid: number, Meetings: Meet[]) => {
-		setNewMeet(Meetings.filter((meet) => meet.uid === uid)[0]);
-		setDialogOpen(true);
-		let newMeetings = Meetings.filter((meet) => meet.uid !== uid);
-		setMeetings(newMeetings);
-		saveData(newMeetings);
-	};
+  const handleDialogOpen = () => {
+    setNewMeet({ name: "", description: "", url: "", uid: 0 });
+    setDialogOpen(true);
+  };
 
-	const handleDialogOpen = () => {
-		setNewMeet({ name: "", description: "", url: "", uid: 0 });
-		setDialogOpen(true);
-	};
+  const [meetings, setMeetings] = useState<Meet[]>(getData());
 
-	const [meetings, setMeetings] = useState<Meet[]>(getData());
+  return (
+    <ThemeProvider theme={theme}>
+      <Box mb={6} pb={6}>
+        <Grid
+          container
+          spacing={3}
+          direction="row"
+          justify="center"
+          alignItems="center"
+        >
+          {meetings.map((meeting) => {
+            return (
+              <Grid key={meeting.uid} item xs={12} sm={12} md={6}>
+                <Box>
+                  <Card elevation={2}>
+                    <CardHeader
+                      title={meeting.name}
+                      action={
+                        <span>
+                          <IconButton
+                            style={{ color: red[600] }}
+                            onClick={() => deleteMeet(meeting.uid, meetings)}
+                            aria-label="meeting options"
+                          >
+                            <DeleteSharp />
+                          </IconButton>
 
-	return (
-		<ThemeProvider theme={theme}>
-			<Box mb={6} pb={6}>
-				<Grid container spacing={3} direction='row' justify='center' alignItems='center'>
-					{meetings.map((meeting) => {
-						return (
-							<Grid key={meeting.uid} item xs={12} sm={12} md={6}>
-								<Box>
-									<Card elevation={2}>
-										<CardHeader
-											title={meeting.name}
-											action={
-												<span>
-													<IconButton
-														style={{ color: red[500] }}
-														onClick={() => deleteMeet(meeting.uid, meetings)}
-														aria-label='meeting options'>
-														<DeleteSharp />
-													</IconButton>
+                          <IconButton
+                            style={{ color: blue[600] }}
+                            onClick={() => editMeet(meeting.uid, meetings)}
+                            aria-label="meeting options"
+                          >
+                            <EditSharp />
+                          </IconButton>
+                        </span>
+                      }
+                    />
+                    <CardContent>
+                      <Typography variant="body1">
+                        {meeting.description}
+                      </Typography>
+                      <Typography variant="h6">
+                        <Link href={"https://meet.google.com" + meeting.url} target="_blank" color="primary">
+                          {meeting.url}
+                        </Link>
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                </Box>
+              </Grid>
+            );
+          })}
+        </Grid>
+      </Box>
 
-													<IconButton
-														style={{ color: blue[500] }}
-														onClick={() => editMeet(meeting.uid, meetings)}
-														aria-label='meeting options'>
-														<EditSharp />
-													</IconButton>
-												</span>
-											}
-										/>
-										<CardContent>
-											<Typography variant='body1'>{meeting.description}</Typography>
-											<Typography variant='h6'>
-												<Link href={meeting.url} color='primary'>
-													{meeting.url}
-												</Link>
-											</Typography>
-										</CardContent>
-									</Card>
-								</Box>
-							</Grid>
-						);
-					})}
-				</Grid>
-			</Box>
+      <Fab
+        color="secondary"
+        aria-label="add-meeting"
+        onClick={handleDialogOpen}
+        className="fab"
+      >
+        <AddSharp></AddSharp>
+      </Fab>
 
-			<Fab color='secondary' aria-label='add-meeting' onClick={handleDialogOpen} className='fab'>
-				<AddSharp></AddSharp>
-			</Fab>
-
-			<Dialog
-				open={dialogOpen}
-				fullWidth
-				maxWidth='md'
-				aria-labelledby='new-meeting'
-				onClose={handleDialogClose}>
-				<DialogTitle id='new-meeting-dialog'>Add Meeting</DialogTitle>
-				<NewMeetingDialogContents
-					handleDialogClose={handleDialogClose}
-					handleDialogSaveAndClose={handleDialogSaveAndClose}
-					newMeet={newMeet}
-					setnewMeet={setNewMeet}
-				/>
-			</Dialog>
-		</ThemeProvider>
-	);
+      <Dialog
+        open={dialogOpen}
+        fullWidth
+        maxWidth="md"
+        aria-labelledby="new-meeting"
+        onClose={handleDialogClose}
+      >
+        <DialogTitle id="new-meeting-dialog">Add Meeting</DialogTitle>
+        <NewMeetingDialogContents
+          handleDialogClose={handleDialogClose}
+          handleDialogSaveAndClose={handleDialogSaveAndClose}
+          newMeet={newMeet}
+          setNewMeet={setNewMeet}
+          editMode={editMode}
+        />
+      </Dialog>
+    </ThemeProvider>
+  );
 }
